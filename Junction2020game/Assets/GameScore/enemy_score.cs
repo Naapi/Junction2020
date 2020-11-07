a32550b726745fe723481a2fbff023b823b176ee
Naapi <terhocraven@gmail.com>
2020-11-08 00:10:42 +0200

    Basic Score texts and background music

---
+++ Junction2020game/Assets/GameScore/enemy_score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemy_score : MonoBehaviour
+{
+     Text txt;
+     private int currentscore=0;
+ 
+     // Use this for initialization
+     void Start () {
+         txt = gameObject.GetComponent<Text>(); 
+         txt.text="Score : " + currentscore;
+     }
+     
+     // Update is called once per frame
+     void Update () {
+         txt.text="Enemy Score : " + currentscore;  
+         currentscore = PlayerPrefs.GetInt("TOTALSCORE"); 
+         PlayerPrefs.SetInt("SHOWSTARTSCORE",currentscore); 
+     }
+}
---
+++ Junction2020game/Assets/GameScore/my_score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class my_score : MonoBehaviour
+{
+     Text txt;
+     private int currentscore=0;
+ 
+     // Use this for initialization
+     void Start () {
+         txt = gameObject.GetComponent<Text>(); 
+         txt.text="Score : " + currentscore;
+     }
+     
+     // Update is called once per frame
+     void Update () {
+         txt.text="My Score : " + currentscore;  
+         currentscore = PlayerPrefs.GetInt("TOTALSCORE"); 
+         PlayerPrefs.SetInt("SHOWSTARTSCORE",currentscore); 
+     }
+}
