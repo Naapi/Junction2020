7f271b853ed444472ee5552aa9c316327cea8c14
mtlukasz <61163947+mtlukasz@users.noreply.github.com>
2020-11-07 18:43:44 +0100

    create Catanks_backend API

---
+++ Catanks_backend/Catanks_backend/models/ScoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catanks_backend.models
+{
+    public class ScoreContext : DbContext
+    {
+        public ScoreContext(DbContextOptions<ScoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Score> Scores { get; set; }
+    }
+}
